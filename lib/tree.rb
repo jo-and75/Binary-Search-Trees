8ec2dc70791c003dad639f556bf5d3fac72796c6
jo-and75 <jaotika@gmail.com>
2024-09-17 11:24:14 +0300

    Add build tree method

--- lib/tree.rb
+++ lib/tree.rb
@@ -5,5 +5,16 @@ class Tree
     @arr = entry.uniq
     @root = build_tree(@arr)
   end
+ 
+  def build_tree(arr, start = 0, finish = arr.length - 1)
+    return if start > finish
+
+    mid = (start + finish) / 2
+    root_node = Node.new(arr[mid])
+
+    root_node.left = build_tree(arr, start, mid - 1)
+    root_node.right = build_tree(arr, mid + 1, finish)
+    root_node
+  end
 
 end
