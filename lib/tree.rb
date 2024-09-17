4cfa18ddf6d32303596d7d78391e17c064b54e60
jo-and75 <jaotika@gmail.com>
2024-09-17 11:24:46 +0300

    Add pretty print method to visualize binary search tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -17,4 +17,9 @@ class Tree
     root_node
   end
 
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
